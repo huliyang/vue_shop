836eae95130b25b3c191d9495df6cc56d1d4339e
huliyang <liyang_hu@outlook.com>
2021-03-09 16:42:42 +0800

    完成分类功能开发

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+//导入Element UI的CSS样式文件
+import 'element-ui/lib/theme-chalk/index.css';
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
 
 // 配置请求的根路径
@@ -17,6 +20,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
 
 new Vue({
   router,
